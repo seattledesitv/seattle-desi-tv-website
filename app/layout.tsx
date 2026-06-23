85ddb3e57282efa82b58ff3b879a4a23fa81ca5a
seattledesitv <seattledesitv@gmail.com>
2026-06-23 10:47:06 -0700

    Show influencer status summary on Event Ops

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata, Viewport } from "next";
 import Script from "next/script";
 import AccessibilityFixes from "./components/AccessibilityFixes";
+import EventOpsInfluencerNotice from "./components/EventOpsInfluencerNotice";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://seattledesitv.com";
 
@@ -114,6 +115,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
       </head>
       <body>
+        <EventOpsInfluencerNotice />
         {children}
         <AccessibilityFixes />
         <Script
