86e2db174cf0d937a6ec59e5f95a8ce2689cc881
seattledesitv <seattledesitv@gmail.com>
2026-06-23 13:29:24 -0700

    Add Event Ops v2 route styling wrapper

---
+++ app/studio/event-ops-v2/layout.tsx
@@ -0,0 +1,5 @@
+import "./event-ops-v2.css";
+
+export default function EventOpsV2Layout({ children }: { children: React.ReactNode }) {
+  return <div className="event-ops-v2-skin">{children}</div>;
+}
