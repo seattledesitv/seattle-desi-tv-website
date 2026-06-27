fcb6f08047aa6d075a5476cfc5485936d4432738
seattledesitv <seattledesitv@gmail.com>
2026-06-27 08:47:25 -0700

    Add Studio community groups page

---
+++ app/studio/community-groups/page.tsx
@@ -0,0 +1,5 @@
+import StudioCommunityManagePage from "../../components/StudioCommunityManagePage";
+
+export default function StudioCommunityGroupsPage() {
+  return <StudioCommunityManagePage kind="groups" />;
+}
