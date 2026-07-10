5a8fd75e0316b47aa6e31e8d008dcae3baf96ea8
seattledesitv <seattledesitv@gmail.com>
2026-07-10 13:28:54 -0700

    Show live preview for hero layout selection

--- app/studio/hero/page.tsx
+++ app/studio/hero/page.tsx
@@ -1,5 +1,6 @@
 import HeroManagementV3 from "./HeroManagementV3";
+import HeroLayoutPreviewBridge from "./HeroLayoutPreviewBridge";
 
 export default function HeroPage() {
-  return <HeroManagementV3 />;
+  return <><HeroManagementV3 /><HeroLayoutPreviewBridge /></>;
 }
