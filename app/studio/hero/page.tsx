17435778d1ff771c71faf68ef277c97096f320d8
seattledesitv <seattledesitv@gmail.com>
2026-07-10 13:44:02 -0700

    Show expanded hero theme choices in Studio

--- app/studio/hero/page.tsx
+++ app/studio/hero/page.tsx
@@ -1,6 +1,7 @@
 import HeroManagementV3 from "./HeroManagementV3";
 import HeroLayoutPreviewBridge from "./HeroLayoutPreviewBridge";
+import HeroThemeOptionInjector from "./HeroThemeOptionInjector";
 
 export default function HeroPage() {
-  return <><HeroManagementV3 /><HeroLayoutPreviewBridge /></>;
+  return <><HeroManagementV3 /><HeroLayoutPreviewBridge /><HeroThemeOptionInjector /></>;
 }
